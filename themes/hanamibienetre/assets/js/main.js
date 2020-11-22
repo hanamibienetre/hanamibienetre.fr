b87ba6f85aa6d85649fd7421b3153d8af0001151
hgouchet <rvflash@users.noreply.github.com>
2020-11-22 18:29:32 +0100

    adds banner

--- themes/hanamibienetre/assets/js/main.js
+++ themes/hanamibienetre/assets/js/main.js
@@ -2,10 +2,18 @@
   const banner = document.getElementById("alert-banner");
   const bannerCloseButton = banner.getElementsByClassName("banner-close");
   const closeBanner = () => {
+    if(typeof sessionStorage != "undefined") {
+      sessionStorage.setItem("banner", false);
+    }
     banner.style.display = "none";
   };
   console.log({ banner }, { bannerCloseButton });
   if (bannerCloseButton) {
     bannerCloseButton[0].addEventListener("click", closeBanner);
+  }
+  if(typeof sessionStorage != "undefined") {
+    if(sessionStorage.getItem("banner")){
+      closeBanner()
+    }
   }
 })()
